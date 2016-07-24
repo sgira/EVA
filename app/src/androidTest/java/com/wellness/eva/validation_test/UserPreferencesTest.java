56c8a2980511e2a66955fa5ab902176f8f4c7ccc
SinJa <sgira005@fiu.edu>
2016-07-24 19:42:07 -0400

    Updated gradle

---
+++ app/src/androidTest/java/com/wellness/eva/validation_test/UserPreferencesTest.java
@@ -0,0 +1,62 @@
+package com.wellness.eva.validation;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by SinJa on 7/24/16.
+ */
+public class UserPreferencesTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testIsAutoCall911() throws Exception {
+
+    }
+
+    public void testSetAutoCall911() throws Exception {
+
+    }
+
+    public void testIsEnglish() throws Exception {
+
+    }
+
+    public void testSetEnglish() throws Exception {
+
+    }
+
+    public void testIsSendBroadcast() throws Exception {
+
+    }
+
+    public void testSetSendBroadcast() throws Exception {
+
+    }
+
+    public void testIsReceiveBroadcast() throws Exception {
+
+    }
+
+    public void testSetReceiveBroadcast() throws Exception {
+
+    }
+
+    public void testGetState() throws Exception {
+
+    }
+
+    public void testSetState() throws Exception {
+
+    }
+
+    public void testAttach() throws Exception {
+
+    }
+
+    public void testNotifyAllObservers() throws Exception {
+
+    }
+}
