86fa3b6df31658fd5200ef2e6d6a464adb15ae0e
SinJa <sgira005@fiu.edu>
2016-07-24 19:26:36 -0400

    Updated gradle

--- app/src/androidTest/java/com/wellness/eva/ApplicationTest.java
+++ app/src/androidTest/java/com/wellness/eva/ApplicationTest.java
@@ -5,8 +5,8 @@ import android.test.ApplicationTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.junit.runner.RunWith;
-
-@SmallTest
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ApplicationTest
 {
 
