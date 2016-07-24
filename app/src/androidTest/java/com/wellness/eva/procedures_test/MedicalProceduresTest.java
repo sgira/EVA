e6c0a98539fd3168e22686db900b01ceb0ce3f6b
SinJa <sgira005@fiu.edu>
2016-07-24 19:07:11 -0400

    Updating resources

---
+++ app/src/androidTest/java/com/wellness/eva/procedures_test/MedicalProceduresTest.java
@@ -0,0 +1,22 @@
+package com.wellness.eva.procedures;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by SinJa on 7/24/16.
+ */
+public class MedicalProceduresTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testOnCreate() throws Exception {
+
+    }
+}
